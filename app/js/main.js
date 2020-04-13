05179f6b588a709f5e51325ec0b93f3ba695e58c
mid12night <atwinternight@gmail.com>
2020-04-13 16:00:35 +0300

    added new-items page

--- app/js/main.js
+++ app/js/main.js
@@ -1,7 +1,5 @@
 $(function () {
 
-
-
     $(".rate-star").rateYo({
         rating: 5,
         starWidth: "12px",
@@ -10,9 +8,31 @@ $(function () {
 
     $('.product-slider__inner').slick({
         dots: true,
-        arrows:false,
+        arrows: false,
         slidesToShow: 4,
         slidesToScroll: 4
+    });
+
+    
+    $(".js-range-slider").ionRangeSlider({
+        type: "double",
+        min: 0,
+        max: 1200,
+        from: 0,
+        to: 600,
+        prefix: "$"
+    });
+    
+    $('.icon-th-list').on('click', function(){
+        $('.product__item').addClass('list');
+        $('.icon-th-list').addClass('active'); 
+        $('.icon-th-large').removeClass('active'); 
+    });
+
+    $('.icon-th-large').on('click', function(){
+        $('.product__item').removeClass('list');
+        $('.icon-th-large').addClass('active');
+        $('.icon-th-list').removeClass('active');
     });
 
     var mixer = mixitup('.products__inner-box');
